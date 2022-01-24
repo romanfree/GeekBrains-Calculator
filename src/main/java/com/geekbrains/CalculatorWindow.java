7a6b6a218e781e5c04e24721128e56a73d16cea6
roman <romanfree1311@gmail.com>
2022-01-24 20:30:09 +0300

    дз 8 урок

--- src/main/java/com/geekbrains/CalculatorWindow.java
+++ src/main/java/com/geekbrains/CalculatorWindow.java
@@ -1,4 +1,24 @@
 package com.geekbrains;
 
-public class CalculatorWindow {
+import javax.swing.*;
+import java.awt.*;
+
+import static java.awt.EventQueue.invokeLater;
+
+public class CalculatorWindow extends JFrame {
+    public CalculatorWindow(){
+        setTitle("Калькулятор");
+        Panel panel = new Panel();
+        add(panel);
+        pack();
+
+        int width = 350;
+        int height = 350;
+        GraphicsDevice gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+        int screenWidth = gd.getDisplayMode().getWidth();
+        int screenHeight = gd.getDisplayMode().getHeight();
+        setBounds(screenWidth / 2 - width / 2, screenHeight / 2 - height / 2, width, height);
+
+    }
+
 }
